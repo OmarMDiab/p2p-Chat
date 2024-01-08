9d73d6956036b90f55f7b006fd1f7f387dc3c243
NourAbouElMakarem <91809854+NourAbouElMakarem@users.noreply.github.com>
2024-01-08 22:47:44 +0200

    Update integration_testing.py
    added def test_create_login_deleteaccount()

--- p2pChat/integration_testing.py
+++ p2pChat/integration_testing.py
@@ -51,6 +51,7 @@ class TestIntegration(unittest.TestCase):
         password = "1234"
         room_name = "testing_room"
         leave_result=1
+        self.peer_main.create_new_account(username, password)
         # Create a new chat room and then attempt to join it from another client
         self.mock_input.side_effect = [2, room_name, password]  
         self.peer_main.chatroom_menu()
@@ -64,7 +65,18 @@ class TestIntegration(unittest.TestCase):
         # Leave the chat room
         self.mock_input.side_effect = [3, room_name, leave_result]  
         self.peer_main.chatroom_menu()
-        leave_result = self.peer_main.leave_room(room_name, username) 
+        leave_result = self.peer_main.leave_room(room_name, username)    
+
+    def test_create_login_deleteaccount(self):
+        username = "test_user"
+        password = "1234"
+        delete_result=1
+        self.peer_main.create_new_account(username, password)
+        # Set up the expected user inputs during the test
+        self.mock_input.side_effect = [username, password, username, password, 1]
+        self.peer_main.select_menu(self, Choice = 5, Title = "Chose to delete account ")
+        delete_result = self.peer_main.delete_account(username)
+        self.assertEqual(delete_result, 1)
 
 if __name__ == '__main__':
     unittest.main()
